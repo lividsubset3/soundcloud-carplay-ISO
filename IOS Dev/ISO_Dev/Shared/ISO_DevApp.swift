67bb3869019b13c7184c0886a38ef70a56899f5b
Pat_SmallMC <lividsubset3@gmail.com>
2021-03-24 23:32:13 -0600

    Start of IOS Dev

---
+++ IOS Dev/ISO_Dev/Shared/ISO_DevApp.swift
@@ -0,0 +1,18 @@
+//
+//  ISO_DevApp.swift
+//  Shared
+//
+//  Created by pat on 3/24/21.
+//
+//
+
+import SwiftUI
+
+@main
+struct ISO_DevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
