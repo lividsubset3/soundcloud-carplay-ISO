48ab0e3316d0da81e1f9cfc6b7dd137d24c3fae9
Pat_SmallMC <lividsubset3@gmail.com>
2021-04-09 23:17:22 -0600

    ++Login button & auth pass/fail

--- IOS Dev/ISO_Dev/Shared/ContentView.swift
+++ IOS Dev/ISO_Dev/Shared/ContentView.swift
@@ -10,19 +10,46 @@ import SwiftUI
 
 let lightGreyColor = Color(red: 239.0 / 255.0, green: 243.0 / 255.0, blue: 244.0 / 255.0, opacity: 1.0)
 
+let storeUser = "Livid"
+let storePass = "pass"
+
 struct ContentView: View {
 
     @State var username: String = ""
     @State var password: String = ""
 
+    @State var authDidFail: Bool = false
+    @State var authDidAcc:  Bool = false
+
     var body: some View {
-        VStack {
-            WelcomeText()
-            UsernameTextField(username: $username)
-            PasswordSecureField(password: $password)
-            Button(action: { print("Button tapped") }) {
-            }
-        }.padding()
+        ZStack {
+            VStack {
+                WelcomeText()
+                UsernameTextField(username: $username)
+                PasswordSecureField(password: $password)
+                if authDidFail {
+                    Text("Auth Fail")
+                            .offset(y: -10)
+                            .foregroundColor(.red)
+                }
+                if authDidAcc {
+                    Text("Auth Succeed!")
+                            .offset(y: -10)
+                            .foregroundColor(.green)
+                }
+                Button(action: {
+                    if self.username == storeUser && self.password == storePass {
+                        self.authDidAcc = true
+                        self.authDidFail = false
+                    } else {
+                        self.authDidFail = true
+                        self.authDidAcc = false
+                    }
+                }) {
+                    LoginButtonContent()
+                }
+            }.padding()
+        }
     }
 }
 
@@ -64,3 +91,16 @@ struct PasswordSecureField: View {
                 .padding(.bottom, 20)
     }
 }
+
+struct LoginButtonContent: View {
+    var body: some View {
+        return Text("LOGIN")
+            .padding()
+            .foregroundColor(.white)
+            .font(.headline)
+            .frame(width: 220, height: 60)
+            .background(Color.green)
+            .cornerRadius(15.0)
+    }
+}
+
